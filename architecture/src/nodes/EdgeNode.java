1a21a2306ebf277bb6f69ccda84c02735f9d91ff
oochiengbrian6-ui <oochieng.brian6@gmail.com>
2026-02-05 13:04:24 +0300

    Add EdgeNode class to handle client requests

---
+++ architecture/src/nodes/EdgeNode.java
@@ -0,0 +1,15 @@
+package nodes;
+
+public class EdgeNode extends Node {
+
+    public EdgeNode(String nodeId, int latency, int throughput,
+                    double packetLoss, int cpuUsage, double memory) {
+        super(nodeId, latency, throughput, packetLoss, cpuUsage, memory);
+    }
+
+    @Override
+    public void handleRequest(String request) {
+        System.out.println(nodeId + " received client request: " + request);
+        System.out.println("Forwarding request to Core layer...");
+    }
+}
